3bcdc42fb1728281614479c2cdf9db6d5a66dfa1
vitorcastro <v170rc@hotmail.com>
2026-09-20 23:32:18 +0200

    fixes: added shipping address fields and billing_address

--- app/services/customer/import/cavegest.rb
+++ app/services/customer/import/cavegest.rb
@@ -27,7 +27,16 @@ class Customer::Import::Cavegest < Importer::Base
         customer_category: N.text(row[20]),
         price_grid_code:   N.text(row[21]),
         vat_number:        N.text(row[23]),
-        excise_number:     N.text(row[24])
+        excise_number:     N.text(row[24]),
+        shipping_last_name:    N.text(row[11]),
+        shipping_first_name:   N.text(row[12]),
+        shipping_company_name: N.text(row[13]),
+        shipping_address1:     N.text(row[14]),
+        shipping_zip:          N.zip(row[15]),
+        shipping_city:         N.text(row[16]),
+        shipping_country_code: N.country_code(row[17]),
+        shipping_phone:        row[18].to_s,
+        use_billing_address:   row[11].nil? && row[14].nil?
       )
 
 
